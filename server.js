c01aa4b7b0c62971a3a8e4fff44a6b11b5b23123
Matthias <matthiasscharnowski@gmail.com>
2019-08-25 11:33:42 +0200

    Added functionality to check for URL, in case documentation is requested, the documentation.html file is the response. The URL and timestamp of the request is logged to a seperate log.txt file

--- server.js
+++ server.js
@@ -14,23 +14,29 @@ http.createServer((request, response) => {
   if (q.pathname.includes('documentation')) {
     filePath = (__dirname + '/documentation.html'); // in case the URL contains the filepath of documentation, this is used as input for readFile. otherwise, index.html is used.
   } else {
-    filePath = 'index.html';
+    filePath = 'index.html'; // in case the URL contains something else, index.html is set as filepath
   };
 
-  fs.readFile(filePath, function(err, data) {
+  fs.readFile(filePath, function(err, data) { // opens the filepath, callback function for error handling
     if (err) {
       throw err;
     }
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
   });
 
-}).listen(8089);
-
-  /*fs.writeFile("log.txt", "Hello world!", function(err) {
-    if(err) {
-        return console.log(err);
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', function(err) { // opens log.txt, adds the URL + timestamp, callback for error handling
+    if (err) {
+      console.log(err);
     }
-    console.log("File saved successfully!");
-});*/
+      else {
+    console.log('Added to log.');
+    }
+  });
+
+}).listen(8093); // listening on port 8093
+
 
 
 
